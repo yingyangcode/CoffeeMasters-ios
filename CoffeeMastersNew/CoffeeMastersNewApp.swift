f6a2105601cc522f27d2ea76eaaf95da96759b66
yingyangcode <marcel.anis.21@gmail.com>
2022-07-10 16:37:25 -0400

    Initial Commit

---
+++ CoffeeMastersNew/CoffeeMastersNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersNewApp.swift
+//  CoffeeMastersNew
+//
+//  Created by Marcel Anis on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
